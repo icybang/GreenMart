9530c3eb5060331156a25c239a024cf1f134bb2c
Akshay Bhujbal <akshaybhujbal31@gmail.com>
2023-02-22 12:45:15 +0530

    adding dto

---
+++ 21-2-2023/greenmart/src/main/java/com/app/dto/ProductDto.java
@@ -0,0 +1,50 @@
+package com.app.dto;
+
+import java.time.LocalDate;
+
+import com.app.pojos.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@JsonInclude(Include.NON_EMPTY)
+public class ProductDto extends BaseEntity{
+
+		private String productName;
+		
+		private double rate;
+		
+		private int discount;
+		
+		private String productDescription;
+		
+		private String image;
+		
+		private LocalDate expiryDate;
+		
+		private int productQuantity;
+		
+		private double averageRating;
+
+		public ProductDto(String productName, double rate, int discount, String productDescription, String image,
+				LocalDate expiryDate, int productQuantity, double averageRating) {
+			super();
+			this.productName = productName;
+			this.rate = rate;
+			this.discount = discount;
+			this.productDescription = productDescription;
+			this.image = image;
+			this.expiryDate = expiryDate;
+			this.productQuantity = productQuantity;
+			this.averageRating = averageRating;
+		}
+		
+}
